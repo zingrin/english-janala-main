8873cd9cf383770e9b2b4a0aed5fcc4d7bf4689f
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-03-16 16:50:24 +0600

    load dynamic lesson btn

--- script/index.js
+++ script/index.js
@@ -23,3 +23,22 @@ logoutButton.onclick =  function () {
         mainSection.classList.add("hidden")
     
 };
+
+const showLessonBtn = async()=>{
+    const res = await fetch('https://openapi.programming-hero.com/api/levels/all')
+    const levels = await res.json();
+    const allLevels = levels.data;
+    const levelContainer = document.getElementById('levelContainer');
+    allLevels.forEach(level => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+         <button
+                    class="btn text-blue-800 border border-blue-500 flex items-center gap-2 px-4 py-2 bg-transparent hover:bg-blue-700 hover:text-white">
+                    <img src="./assets/fa-book-open.png" alt="" class="w-5 h-5 hover:text-white">
+                    Lesson -${level.level_no}
+                </button>
+        `
+        levelContainer.appendChild(div)
+    });
+}
+showLessonBtn()
